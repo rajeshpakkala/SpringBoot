c6ee9942b63738e6144234a5d0fee3f52fc036c6
rajeshpakkala <rajeshpakkala786@gmail.com>
2024-05-27 21:19:59 +0530

    spring school db

---
+++ src/SchoolApplication.java
@@ -0,0 +1,14 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+package com.example.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolApplication.class, args);
+
+        }
+    }
